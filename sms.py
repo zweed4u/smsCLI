d9368ad1e3174c56b12196a9862863518a7106ab
Zachary Weeden <zdw7287@rit.edu>
2017-01-21 22:19:08 -0500

    Added support for basic authentication feature

--- sms.py
+++ sms.py
@@ -4,8 +4,11 @@
 #flag toggle for sending only one automated message - sms bomb flag in config add conditional to see if no dup flag needed
 #exception handling needed in post requests.exceptions.ConnectionError: ('Connection aborted.', BadStatusLine("''",))
 #...paramiko.ssh_exception.NoValidConnectionsError  - ensure that devices ip address is correct
+#exception handling needed for authentication issues - silent fails (check response after request for 200)
+#package handles toggling authentication very shakily - one or the other
 import os, sys, time, datetime, requests, threading, paramiko, ConfigParser
 from scp import SCPClient
+from requests.auth import HTTPDigestAuth
 print
 
 # Get the project directory to avoid using relative paths
@@ -18,13 +21,16 @@ c.read(configFilePath)
 
 class Config:
     # Pull user info 
-    deb = c.get('install','debNeeded')
+    debNeeded = c.get('install','debNeeded')
     number = c.get('number','number')
     ip = c.get('ssh', 'ip')
-    pwd = c.get('ssh','password')
+    sshPass = c.get('ssh','password')
     udid = c.get('device','udid')
     automationNeeded = c.get('automated','needed')
     automatedTime = c.get('automated','time')
+    authenticationNeeded = c.get('authentication','authNeeded')
+    authUser = c.get('authentication', 'username')
+    authPass = c.get('authentication', 'pass')
 
 class SSH:
 	ssh=paramiko.SSHClient()
@@ -40,7 +46,7 @@ class SSH:
 print 'Checking config for deb install param...'
 print
 user_config = Config()
-if user_config.deb.lower() == 'true':
+if user_config.debNeeded.lower() == 'true':
 	cydiaSession=requests.session()
 	remoteMessageIOS9Deb='http://fay.insanelyi.com/pool/free/c/com.sull.remotemessagesios9/com.sull.remotemessagesios9_3.2.1_iphoneos-arm.deb'
 	cydiaHeaders={
@@ -59,7 +65,7 @@ if user_config.deb.lower() == 'true':
 			if chunk: # filter out keep-alive new chunks
 				f.write(chunk)
 	print 'SSHing into device using config vals...'
-	local_ssh = SSH(user_config.ip, 22, 'root', user_config.pwd)
+	local_ssh = SSH(user_config.ip, 22, 'root', user_config.sshPass)
 	local_ssh.connect()
 	scp = SCPClient(local_ssh.ssh.get_transport())
 	print 'SCPing downloaded deb to device...'
@@ -74,14 +80,21 @@ if user_config.deb.lower() == 'true':
 else:
 	print 'Deb installation is not needed...'
 print
+#Prompt to make changes to authentication prior to enabling/reenabling feature 
 print 'Go into Settings > Remote Messages > Enable [ON]'
-print 'Ensure that "Use Authentication" is disabled'
+print '[CONFIG] '+user_config.ip+' is listed as the IP. Ensure that this is listed as "Current IP"'
+if user_config.authenticationNeeded.lower()=='true':
+	print '[CONFIG] Authentication required! Using provided credentitals from config! Ensure values in config and Settings app match'
+else:
+	print '[CONFIG] Authentication not needed. Ensure that "Use Authentication" is disabled'
 print 'Ensure that the server port is: 333'
 print
 raw_input('Confirm the above is True...')
 print
+
 hashId='SMS;-;+'+user_config.number #'SMS' or 'iMessage'(self) prepended
 
+#params dict used?
 params={
 	'hashid':hashId,
 	'date':'0',
@@ -91,7 +104,7 @@ params={
 
 messageSent=0   #only send message once - midnight checker toggle flag on midnoght detect conditional
 
-def automatedTimeMessage(time, noDupMessage):
+def automatedTimeMessage(time, noDupMessage, authNeeded, authUser, authPass):
 	global messageSent
 	while 1: #and if flag = 0
 		if datetime.datetime.now().strftime("%I:%M%p") == time and noDupMessage==0:
@@ -103,7 +116,10 @@ def automatedTimeMessage(time, noDupMessage):
 				'file-name': (None, ''),
 				'text': (None, message)
 			}
-			session.post('http://'+user_config.ip+':333/sendMessage.srv',files=files)
+			if authNeeded.lower()=='true':
+				session.post('http://'+user_config.ip+':333/sendMessage.srv',files=files, auth=HTTPDigestAuth(authUser, authPass))
+			else:
+				session.post('http://'+user_config.ip+':333/sendMessage.srv',files=files)
 			messageSent=1
 			noDupMessage=1
 			print 'Background conditional thread hit!'
@@ -121,7 +137,7 @@ def becauseItsMidnight():
 if user_config.automationNeeded.lower() == 'true':
 	print 'Automated messaging set!'
 	print 'Messaging '+user_config.number+' at '+user_config.automatedTime
-	automatedMessageThread = threading.Thread(target=automatedTimeMessage, args=(user_config.automatedTime,messageSent,))
+	automatedMessageThread = threading.Thread(target=automatedTimeMessage, args=(user_config.automatedTime,messageSent,user_config.authenticationNeeded,user_config.authUser,user_config.authPass,))
 	automatedMessageThread.daemon = True
 	automatedMessageThread.start()
 	newDayDetectonResetFlag = threading.Thread(target=becauseItsMidnight)
@@ -141,4 +157,7 @@ while 1:
 		'file-name': (None, ''),
 		'text': (None, message)
 	}
-	session.post('http://'+user_config.ip+':333/sendMessage.srv',files=files)
+	if user_config.authenticationNeeded.lower()=='true':
+		session.post('http://'+user_config.ip+':333/sendMessage.srv',files=files, auth=HTTPDigestAuth(user_config.authUser, user_config.authPass))
+	else:
+		session.post('http://'+user_config.ip+':333/sendMessage.srv',files=files)
